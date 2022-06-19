dfdb72c53efa59e89eacf620912bf9b46eb3ecdc
kullken <karl.viktor.kull@gmail.com>
2022-06-19 17:27:52 +0200

    working send a recive

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
+#include <iostream>
+
+#include<unistd.h>
+
 #include "SerialCommunication/serial.hpp"
 
 int main() {
 
-    SerialC::SerialConnection a = SerialC::SerialConnection("/dev/ttyUSB0");
-    int k = 100;
-    for(int i = 0; i < k; i++){
-        a.writeM("hello world!");
+    SerialC::SerialConnection a = SerialC::SerialConnection("/dev/ttyACM0");
+
+    usleep(2 * 1000000);
+
+    a.writeM("$0,0,0,1,0,0,0,0,0,0#");
+
+    for(int i = 0; i < 100000; i++){
+        usleep(1 * 1000000);
+        auto retval = a.readM();
+        std::cout << retval << std::endl;
     }
-    a.writeM("hello world!");
+
 
     //https://blog.mbedded.ninja/programming/operating-systems/linux/linux-serial-ports-using-c-cpp/
 
